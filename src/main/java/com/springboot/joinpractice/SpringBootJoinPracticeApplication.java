5b02ea29e39e3ef84f85c1e4e6ac384a72b662fd
minkyoung kim <minry20415@naver.com>
2022-12-04 20:20:30 +0900

    회원가입 api 구현 - join(회원가입)

---
+++ src/main/java/com/springboot/joinpractice/SpringBootJoinPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.joinpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJoinPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJoinPracticeApplication.class, args);
+	}
+
+}
